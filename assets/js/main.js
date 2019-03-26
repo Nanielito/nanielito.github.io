d9087b752eb7a1c92c8804a518b0589d9d39782e
daniel.ramirez <daniel.ramirez@smartmatic.com>
2019-03-26 18:36:00 -0400

    #M Resume site was updated

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,78 +1,70 @@
 $(function() {
-  var language = ((new URL(document.location)).searchParams).get('setLng') || navigator.language || navigator.userLanguage;
-    
-  var option = {
-    lng: (/^es-\d+$/).test(language) ? 'es' : language.split('-')[0],
-    resGetPath: 'resources/locales/__lng__/__ns__.json',
-    fallbacking: 'en'
-  };
-    
-  $.i18n.init(option, function () {
-    $('[id$="_section"]').i18n();
-    return false;
-  });
 
-  $('.bfh-languages').on('click', function (e) {
-    e.preventDefault();
-
-    var language = $(this).attr('data-language').split('_')[0];
-      
-    window.location.replace(document.location.origin + '?setLng=' + language);
-
-    return false;
-  });
-
-  /*======= Skillset *=======*/
+    var browserLang = navigator.language || navigator.userLanguage;
     
-  $('.level-bar-inner').css('width', '0');
+    var option = {
+        lng: (/^es-\d+$/).test(browserLang) ? 'es' : browserLang,
+        resGetPath: 'resources/locales/__lng__/__ns__.json',
+        fallbacking: 'es'
+    };
     
-  $(window).on('load', function () {
-    $('.level-bar-inner').each(function () {
-      var itemWidth = $(this).data('level');
-  
-      $(this).animate({
-        width: itemWidth
-      }, 800);
+    $.i18n.init(option, function() {
+        $('[id$="_section"]').i18n();
     });
+    
+    /*======= Skillset *=======*/
+    
+    $('.level-bar-inner').css('width', '0');
+    
+    $(window).on('load', function() {
 
-    return false;
-  });
+        $('.level-bar-inner').each(function() {
+        
+            var itemWidth = $(this).data('level');
+            
+            $(this).animate({
+                width: itemWidth
+            }, 800);
+            
+        });
+
+    });
     
-  /* Bootstrap Tooltip for Skillset */
-  $('.level-label').tooltip();
+    /* Bootstrap Tooltip for Skillset */
+    $('.level-label').tooltip();
     
-  /* jQuery RSS - https://github.com/sdepold/jquery-rss */
-  $("#rss-feeds").rss(
+    /* jQuery RSS - https://github.com/sdepold/jquery-rss */
+    $("#rss-feeds").rss(
     
-    //Change this to your own rss feeds
-    "http://feeds.feedburner.com/TechCrunch/startups",
+        //Change this to your own rss feeds
+        "http://feeds.feedburner.com/TechCrunch/startups",
         
-    {
-      // how many entries do you want?
-      // default: 4
-      // valid values: any integer
-      limit: 3,
+        {
+        // how many entries do you want?
+        // default: 4
+        // valid values: any integer
+        limit: 3,
         
-      // the effect, which is used to let the entries appear
-      // default: 'show'
-      // valid values: 'show', 'slide', 'slideFast', 'slideSynced', 'slideFastSynced'
-      effect: 'slideFastSynced',
+        // the effect, which is used to let the entries appear
+        // default: 'show'
+        // valid values: 'show', 'slide', 'slideFast', 'slideSynced', 'slideFastSynced'
+        effect: 'slideFastSynced',
         
-      // outer template for the html transformation
-      // default: "<ul>{entries}</ul>"
-      // valid values: any string
-      layoutTemplate: "<div class='item'>{entries}</div>",
+        // outer template for the html transformation
+        // default: "<ul>{entries}</ul>"
+        // valid values: any string
+        layoutTemplate: "<div class='item'>{entries}</div>",
         
-      // inner template for each entry
-      // default: '<li><a href="{url}">[{author}@{date}] {title}</a><br/>{shortBodyPlain}</li>'
-      // valid values: any string
-      entryTemplate: '<h3 class="title"><a href="{url}" target="_blank">{title}</a></h3><div><p>{shortBodyPlain}</p><a class="more-link" href="{url}" target="_blank"><i class="fa fa-external-link"></i>Read more</a></div>'
+        // inner template for each entry
+        // default: '<li><a href="{url}">[{author}@{date}] {title}</a><br/>{shortBodyPlain}</li>'
+        // valid values: any string
+        entryTemplate: '<h3 class="title"><a href="{url}" target="_blank">{title}</a></h3><div><p>{shortBodyPlain}</p><a class="more-link" href="{url}" target="_blank"><i class="fa fa-external-link"></i>Read more</a></div>'
         
-    }
-  );
+        }
+    );
     
-  /* Github Activity Feed - https://github.com/nanielito/github-activity */
-  GitHubActivity.feed({ username: "nanielito", selector: "#ghfeed" });
-  
-  return false;
+    /* Github Activity Feed - https://github.com/nanielito/github-activity */
+    GitHubActivity.feed({ username: "nanielito", selector: "#ghfeed" });
+
+
 });
